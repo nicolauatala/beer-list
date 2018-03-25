e9934bfd2be00c689c77b1cd596c5bbcb228d0a7
Nicolau Atala <nicolau.atala@hotmail.com>
2018-03-24 21:43:04 -0300

    Implement APIManager

---
+++ BeerList/Modules/BeersList/BeerListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BeerListViewController.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 23/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import UIKit
+
+class BeerListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BeerList/Utils/Constants/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 23/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import Foundation
