236fb74c153e3a3f74e3f3e20880ec98191fe720
Nicolau Atala <nicolau.atala@hotmail.com>
2018-03-29 11:58:25 -0300

    Added notFoundTableViewCell

---
+++ BeerList/Modules/BeerDetail/Presenter/BeerDetailPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  BeerDetailPresenter.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 29/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import Foundation
---
+++ BeerList/Utils/UIViews/TableViewCells/NotFound/NotFoundTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotFoundTableViewCell.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 29/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import UIKit
+
+class NotFoundTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
