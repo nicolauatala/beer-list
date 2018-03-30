edea644ba611c1f0eabf853d6d5b79115b956da0
Nicolau Atala <nicolau.atala@hotmail.com>
2018-03-30 12:05:20 -0300

    Added extension

---
+++ BeerList/Utils/Extension/UILabel+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UILabel+Extension.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 30/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import Foundation
---
+++ BeerList/Utils/Extension/ViewUtil.swift
@@ -0,0 +1,9 @@
+//
+//  ViewUtil.swift
+//  BeerList
+//
+//  Created by Nicolau Atala Pelluzi on 30/03/18.
+//  Copyright © 2018 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import Foundation
